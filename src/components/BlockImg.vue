68253c0badd04221abf57ff6ce5b34239a0c7b14
Jecjvorobey <jeckvorobey69@gmail.com>
2021-07-30 01:40:18 +0300

    сделал попап

--- src/components/BlockImg.vue
+++ src/components/BlockImg.vue
@@ -1,18 +1,38 @@
 <template lang="pug">
   .elements 
     .elements__1 
-    figure.elements__figure
-      img(src="../assets/images/home/img.png" alt="img").elements__img 
+    figure.elements__figure 
+      img(src="../assets/images/home/img.png" alt="img" v-on:click="showImg").elements__img 
     .elements__2 
     .elements__3 
+    popup(v-if="popupShow" v-on:popapClose="popapClose")
+      img(src="../assets/images/home/img.png" alt="img").img-fluid
 </template>
-
 <script>
+import popup from "../components/Popup";
+
 export default {
   name: "BlockImg",
+  components: {
+    popup,
+  },
+
+  data() {
+    return {
+      popupShow: false,
+    };
+  },
+
+  methods: {
+    showImg() {
+      this.popupShow = true;
+    },
+    popapClose() {
+      this.popupShow = false;
+    },
+  },
 };
 </script>
-
 <style lang="scss" scoped>
 @import "../assets/style/_blockimg.scss";
 </style>
